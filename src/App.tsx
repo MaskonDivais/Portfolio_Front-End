36fddc79c88a9969bea07612e0cedfc1d46021c1
Oleksandr Doroshkevich <145459470+MaskonDivais@users.noreply.github.com>
2024-01-08 19:28:35 +0100

    Stractures

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import Menu from './Components/Menu/Menu'
+
 import './style.css';
+import Start from './Components/Start/Start';
+import Play from './Components/Play/Play';
+import Ander from './Components/Ander/Ander';
 
 const App = () => {
   return (
     <div className='container'>
-      <Menu/>
+      <Start/>
+      <Play/>
+      <Ander/>
     </div>
   )
 }
---
+++ src/Components/Ander/Ander.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import module from '../Ander/Andre.module.css'
+
+const Ander = () => {
+  return (
+    <div className={module.container}>
+        
+    </div>
+  )
+}
+
+export default Ander
