e0c0dcc8a7e4aeb65aa57771ce658b4ccd6677d1
Oleksandr Doroshkevich <145459470+MaskonDivais@users.noreply.github.com>
2024-01-19 21:01:50 +0100

    V0.28

--- src/Components/Ander/Ander.tsx
+++ src/Components/Ander/Ander.tsx
@@ -8,16 +8,22 @@ const Ander: React.FC = () => {
   return (
     <div className={module.container}>
       <div className={module.fon}>
-        <div className={module.slider}><HorizontalSlider/></div>
-        <div className={module.footer}>
-          <div className={module.ContButton}><AnderButton/></div>
-          <div className={module.Author}></div>
-          <div className={module.FIcon}></div>
 
-          <p className={module.fp1}></p>
-          <div className={module.FooterLogo}></div>
-          <p className={module.fp2}></p>
+        <div className={module.slider}><HorizontalSlider/></div>
+
+        <div className={module.contfooter}>
+          <div className={module.footer}>
+            <div className={module.ContButton}><AnderButton/></div>
+            <div className={module.Author}></div>
+            <div className={module.FIcon}></div>
+
+            <p className={module.fp1}>This application is an audio player designed for personal use and to provide information about the programming level and design abilities of the developer.</p>
+            <div className={module.FooterLogo}></div>
+            <p className={module.fp2}>All rights reserved. The content of this application is used for educational purposes only and cannot be used for commercial purposes.</p>
+          </div>
         </div>
+
+
       </div>
     </div>
   );
