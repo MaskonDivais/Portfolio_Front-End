34c2f89a6254841008987112d422fdd63ace5899
Oleksandr Doroshkevich <145459470+MaskonDivais@users.noreply.github.com>
2024-07-15 13:50:34 +0200

    Exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,111 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './Pages/MainPage/Main';
-import UserPage from './Pages/UserPage/UserPage';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from './Components/ThemeContext';
-import Login from './Components/LogIn/Login';
+import './style.css';
+
+import Start from '../src/Components/Start/Start';
+import Play from '../src/Components/Play/Play';
+import News from '../src/Components/News/News';
+import Menu from '../src/Components/Menu/Menu';
+import Ander from '../src/Components/Ander/Ander';
+import Navigation from '../src/UI/Navigation/Navigation';
+import Login from '../src/Components/LogIn/Login';
 
 const App: React.FC = () => {
+  const slideTitles = [
+    'Main Page',
+    'Player',
+    'Actual News',
+    'Technical Support'
+  ];
+
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [lettersVisible, setLettersVisible] = useState<number>(0);
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
+
+  // Start Scroll
+  const nextSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide + 1) % 4);
+  };
+
+  const prevSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide - 1 + 4) % 4);
+  };
+
+  const handleScroll = (event: WheelEvent) => {
+    const delta = event.deltaY;
+    if (delta > 0) {
+      nextSlide();
+    } else if (delta < 0) {
+      prevSlide();
+    }
+  };
+
+  useEffect(() => {
+    const handleScrollWrapper = (event: Event) => {
+      if (event instanceof WheelEvent) {
+        handleScroll(event);
+      }
+    };
+    window.addEventListener('wheel', handleScrollWrapper);
+    return () => {
+      window.removeEventListener('wheel', handleScrollWrapper);
+    };
+  }, []);
+  // End Scroll
+
+  // Start isLoading
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+  // End isLoading
+
+  // Start Animation Load
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLettersVisible((prevLettersVisible) => prevLettersVisible + 1);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [lettersVisible]);
+
+  const word = 'MASFLEX';
+
+  if (isLoading) {
+    return (
+      <div className="loadingAnimation">
+        {word.split('').map((letter, index) => (
+          <span key={index} className="loadingText" style={{ visibility: index < lettersVisible ? 'visible' : 'hidden' }}>
+            {letter}
+          </span>
+        ))}
+      </div>
+    );
+  }
+  // End Animation Load
 
   return (
     <ThemeProvider>
-      <Router>
-          <Routes>
-            <Route path="/main" element={<Main />} />
-            <Route path="/user" element={<UserPage />} />
-          </Routes>
-      </Router>
+      <div className="container">
+        {isAuthenticated ? (
+          <div className="slides">
+            {activeSlide === 0 && <Start setActiveSlide={setActiveSlide} />}
+            {activeSlide === 1 && <Play />}
+            {activeSlide === 2 && <News />}
+            {activeSlide === 3 && <Ander />}
+          </div>
+        ) : (
+          <Login setIsAuthenticated={setIsAuthenticated} />
+        )}
+
+        <Navigation activeSlide={activeSlide} nextSlide={nextSlide} prevSlide={prevSlide} />
+
+        <Menu slideTitle={slideTitles[activeSlide]} />
+      </div>
     </ThemeProvider>
   );
 };
 
 export default App;
+
